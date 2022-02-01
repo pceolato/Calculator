b2c60cf668405aff6aeb5ba055daaa722769a8b3
pceolato <pedroceolato2002@gmail.com>
2022-02-01 19:44:25 -0300

    Atualização

---
+++ script.js
@@ -0,0 +1,23 @@
+function insert (num) {
+  let number = document.getElementById('result').innerHTML;
+  document.getElementById('result').innerHTML = number + num;
+}
+
+function clean () {
+  document.getElementById('result').innerHTML = "";
+}
+
+function back() {
+  let result = document.getElementById('result').innerHTML;
+  document.getElementById('result').innerHTML = result.substring(0, result.length -1)
+}
+
+function calculate() {
+  let result = document.getElementById('result').innerHTML;
+  if(result) {
+    document.getElementById('result').innerHTML = eval(result);
+  }
+  else {
+    document.getElementById('result').innerHTML = ""
+  }
+}
